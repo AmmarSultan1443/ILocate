0734d25f5732b459dbffa0a4ecb9ab6b44558d91
ammarsultan <ammar.sultan@edu.umi.ac.ma>
2023-02-26 00:17:45 +0100

    start working on the iLocate project with information that is the project I was assigned to work on during my study at ESTM in 2015 as final class project (PFE) and now I want to rebuild in from scrach because at the first time I didn't write any line of code and it was told that I was part of it, which is true but didn't work on the coding part.

--- app/src/main/java/com/example/ilocate/MainActivity.java
+++ app/src/main/java/com/example/ilocate/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.ilocate;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Timer;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
